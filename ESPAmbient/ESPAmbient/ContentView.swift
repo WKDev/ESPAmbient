7720b3ff6a02663443a033752e9a88d0fdf5aee8
WKDev <chanhyukman@gmail.com>
2022-04-23 17:22:15 +0900

    simplificated the code.

---
+++ ESPAmbient/ESPAmbient/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ESPAmbient
+//
+//  Created by Chanhyeok Son on 2022/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ESPAmbient/ESPAmbient/ESPAmbientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESPAmbientApp.swift
+//  ESPAmbient
+//
+//  Created by Chanhyeok Son on 2022/04/21.
+//
+
+import SwiftUI
+
+@main
+struct ESPAmbientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
